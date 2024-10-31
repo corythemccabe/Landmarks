9b7589dda5206a911ec2da38c96cbcb1ab4871d0
Cory McCabe <83873828+corythemccabe@users.noreply.github.com>
2024-10-30 19:13:45 -0500

    Created Hike struct in Models
    Also added hikeData.json to the ModelData for decoding data from the json file.

---
+++ Landmarks/Models/Hike.swift
@@ -0,0 +1,31 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by Cory McCabe on 10/30/24.
+//
+
+import Foundation
+
+struct Hike: Codable, Identifiable, Hashable {
+    let id: Int
+    let name: String
+    let distance: Double
+    let difficulty: Int
+    let obervations: [Observation]
+    
+    static var formatter = LengthFormatter()
+        
+    var distanceText: String {
+        Hike.formatter.string(fromValue: distance, unit: .kilometer)
+    }
+    
+    struct Observation: Codable, Hashable {
+        let distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+    
+}
--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -10,6 +10,7 @@ import Foundation
 @Observable
     class ModelData {
         var landmarks: [Landmark] = load("landmarkData.json")
+        var hikes: [Hike] = load("hikeData.json")
     }
 
 /// This Function is for reading json data.
